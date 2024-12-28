5223566790c73978b94239997185004a6411c0e3
Akshit-ja <akshitjain20048@gmail.com>
2024-12-28 10:43:02 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+ const accountId = 12345
+ let  accountemail = "akshit@1234.com"
+ var   accountpassword = "12345"
+
+ accountcity = "jaipur"
+
+  accountemail = "hitesh234@.com"
+  accountpassword = "23456"
+  accountcity = "bengaluru"
+
+ console.table(
+    [accountId , accountemail, accountpassword , accountcity]);
